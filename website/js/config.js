5cc6bb7fb52d37c31e24f6d50ab6605c4d5fee5c
karthickgandhiTV <139859343+karthickgandhiTV@users.noreply.github.com>
2024-06-12 18:12:29 +0200

    Config updated

--- website/js/config.js
+++ website/js/config.js
@@ -1,13 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-central-1_W5KTSaTEd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6dr01enpgi8n8mb3o3fgjo68qs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-central-1' // e.g. us-east-2
+        userPoolId: 'us-east-1_JwOSumEmA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3v2lj4rl0fnr5j8i3ls3590okg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://m6kcyp2gx0.execute-api.eu-central-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://a79zsxi1z9.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'karthickgandhi@karthickgandhi.com' ///
+        email: 'karthick@kartechworks.com' ///
     }
 };
